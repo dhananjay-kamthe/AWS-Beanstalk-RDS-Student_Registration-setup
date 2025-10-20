0afff5927368f6ee2f0d9fcf6c6122d008b44145
dhananjay-kamthe <dhananjaykamthe2@gmail.com>
2025-10-20 13:31:08 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,39 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$website = $_POST["website"];
+$comment = $_POST["comment"];
+$gender = $_POST["gender"];
+
+echo $name;
+echo $email;
+echo $gender;
+echo $comment;
+echo $website;
+
+$servername = "awseb-e-8t6xh9mfyk-stack-awsebrdsdatabase-puq95rawvkix.cx684ems4ryy.eu-north-1.rds.amazonaws.com";
+$username = "root";
+$password = "pass1234";
+$dbname = "facebook";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Insert data
+$sql = "INSERT INTO users (name, email, website, comment, gender) 
+        VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
